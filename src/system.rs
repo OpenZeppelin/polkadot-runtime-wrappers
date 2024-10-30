6293703fd8455d041c227d4e4fd24e04cfd35627
4meta5 <asinghchrony@protonmail.com>
2024-10-30 17:15:25 -0400

    move sessions impl to consensus grouping for tanssi integration

--- src/system.rs
+++ src/system.rs
@@ -284,29 +284,5 @@ macro_rules! impl_openzeppelin_system {
             /// Rerun benchmarks if you are making changes to runtime configuration.
             type WeightInfo = weights::pallet_multisig::WeightInfo<Runtime>;
         }
-
-        parameter_types! {
-            // pallet_session ends the session after a fixed period of blocks.
-            // The first session will have length of Offset,
-            // and the following sessions will have length of Period.
-            // This may prove nonsensical if Offset >= Period.
-            pub const Period: u32 = 6 * HOURS;
-            pub const Offset: u32 = 0;
-        }
-
-        impl pallet_session::Config for Runtime {
-            type Keys = SessionKeys;
-            type NextSessionRotation = pallet_session::PeriodicSessions<Period, Offset>;
-            type RuntimeEvent = RuntimeEvent;
-            // Essentially just Aura, but let's be pedantic.
-            type SessionHandler = <SessionKeys as sp_runtime::traits::OpaqueKeys>::KeyTypeIdProviders;
-            type SessionManager = CollatorSelection;
-            type ShouldEndSession = pallet_session::PeriodicSessions<Period, Offset>;
-            type ValidatorId = <Self as frame_system::Config>::AccountId;
-            // we don't have stash and controller, thus we don't need the convert as well.
-            type ValidatorIdOf = pallet_collator_selection::IdentityCollator;
-            /// Rerun benchmarks if you are making changes to runtime configuration.
-            type WeightInfo = weights::pallet_session::WeightInfo<Runtime>;
-        }
     };
 }
